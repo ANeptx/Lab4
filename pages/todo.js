89cd635163e0488911795b020145fc3ea8b37f4b
ANeptx <s6135512056@phuket.psu.ac.th>
2022-02-22 18:28:34 +0700

    Checkpoint1-4

--- pages/todo.js
+++ pages/todo.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const Todo = () => {
+const Todo = ({ avatar_url, login }) => {
 
     const initTasks = [
-        {id: 1, name: "Read a book"},
-        {id: 2, name: "Do homework"},
+        { id: 1, name: "Read a book" },
+        { id: 2, name: "Do homework" },
     ];
 
     const [name, setName] = useState('');
@@ -13,12 +13,20 @@ const Todo = () => {
 
     const add = () => {
         console.log("Add");
-        setTasks([...tasks, { id: tasks[tasks.length-1].id+1 , name: name }]);
+        if (name == "")
+        return;
+        if(tasks.length>=10)
+        return;
+        if(tasks.length==0)
+        setTasks([{ id: 1, name}]);
+        else
+            setTasks([...tasks, { id: tasks[tasks.length-1].id + 1, name: name }])
+          console.log('new task:', tasks);
     }
-    
+
     const del = (id) => {
         console.log('Delete');
-        const newTasks = tasks.filter( (task) => ( +task.id !== +id));
+        const newTasks = tasks.filter((task) => (+task.id !== +id));
         setTasks(newTasks);
     }
 
@@ -28,47 +36,66 @@ const Todo = () => {
         const t = tasks.find((task) => (+task.id === +id))
         setName(t.name);
         //press edit again
-        if ( +idEdit === +id) {
-            const newTasks = tasks.map ((task) => {
-                if( +task.id === +id)
+        if (+idEdit === +id) {
+            const newTasks = tasks.map((task) => {
+                if (+task.id === +id)
                     task.name = name;
                 return task;
-            } )
+            })
             setTasks(newTasks)
             setIdEdit(0);
         }
 
     }
-    
+
     const renderTasks = () => {
-        return tasks.map ((item, index) => {
-            return (<li key={index}>
-                {item.id}
-                {
-                     (idEdit !== item.id) ? 
-                     item.name :
-                     <input type="text" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value)}/>
-                }
-                <button onClick={() => del(item.id)} >Delete</button>
-                <button onClick={() => editTask(item.id)}>Edit</button>
+        return tasks.map((item, index) => {
+            return (<li className="m-4 border-2 border-dashed p-8" key={index}>
+                <div className="text-3xl text-indigo-200 drop-shadow-lg">
+                    <div className="absolute bottom-0 right-0 text-xl mr-2 text-indigo-500">
+                        {index+1}
+                    </div>
+                    {
+                        (idEdit !== item.index) ?
+                            <div className="text-3xl text-indigo-800 drop-shadow-lg drop-shadow-lg max-w-xs">{item.name}</div> :
+                            <input className="text-3xl text-indigo-800" type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)} />
+                    }
+                </div>
+
+                <div className="mt-8 flex justify-center">
+                    <button className="mr-4 drop-shadow-lg bg-red-300 text-indigo-900 hover:bg-red-200 rounded-lg p-4" onClick={() => del(item.id)} >Delete</button>
+                    <button className="mr-4 drop-shadow-lg bg-yellow-300 text-indigo-900 hover:bg-yellow-200 rounded-lg p-4" onClick={() => editTask(item.id)}>Edit</button>
+                </div>
+
             </li>)
         })
     }
 
-    return (<div className="bg-indogo-400 h-screen flex flex-col items-center p-8" >
-    <h1 className="text-3xl text-indigo-200">Todo</h1>
-    <div className="flex items-center m-4 border-4">
-        <input className="rounded-lg text-3xl mt-2" type="text" name="name" onChange={(e) => setName(e.target.value)}/>
-        <button className="bg-green-400 text-indigo-800 mb-2 p-2" onClick={add} >Add</button> {name}
-    </div>
-    <ul>
-    {
-        renderTasks()
-    }
-    </ul>
+    return (<div className="bg-indigo-400 h-screen flex flex-col items-center p-8" >
+        <div className="flex items-center">
+            <h1 className= "m-4 text-indigo-800 text-4xl drop-shadow-lg">
+                <img className="rounded-full inline mr-4" src={avatar_url} width="80" />
+                Todo  for <span>{login} </span>
+            </h1>
+        </div>
+        {/* <h1 className="m-2 text-indigo-800 text-4xl drop-shadow-lg">Todo</h1> */}
+        <div className="flex items-center m-4">
+            <input className="rounded-lg text-green-800 text-4xl drop-shadow-lg pl-2 ml-2 mr-4" type="text" name="name" onChange={(e) => setName(e.target.value)} />
+            <button className="bg-green-600 text-indigo-200 hover:bg-green-500 p-2 rounded-lg" onClick={add} >Add</button>
+        </div>
+        <ul className="flex flex-wrap">
+            {renderTasks()}
+        </ul>
     </div>)
 }
+
+Todo.getInitialProps = async (ctx) => {
+    const res = await fetch('https://api.github.com/users/Aneptx')
+    const json = await res.json()
+    return { login: json.login, avatar_url: json.avatar_url }
+}
+
 
 export default Todo
