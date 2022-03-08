a58d604d13f916c14f21914804a82cc25229c70c
ANeptx <s6135512056@phuket.psu.ac.th>
2022-03-08 17:26:03 +0700

    EditAlert

--- pages/todo.js
+++ pages/todo.js
@@ -14,14 +14,14 @@ const Todo = ({ avatar_url, login }) => {
     const add = () => {
         console.log("Add");
         if (name == "")
-        return;
-        if(tasks.length>=10)
-        return;
-        if(tasks.length==0)
-        setTasks([{ id: 1, name}]);
+            return alert("กรุณากรอกข้อมูลในช่องว่าง");
+        if (tasks.length >= 10)
+            return alert("ไม่สามารถเพิ่มข้อมูลเกิน 10 ครั้ง");
+        if (tasks.length == 0)
+            setTasks([{ id: 1, name }]);
         else
-            setTasks([...tasks, { id: tasks[tasks.length-1].id + 1, name: name }])
-          console.log('new task:', tasks);
+            setTasks([...tasks, { id: tasks[tasks.length - 1].id + 1, name: name }])
+        console.log('new task:', tasks);
     }
 
     const del = (id) => {
@@ -51,16 +51,16 @@ const Todo = ({ avatar_url, login }) => {
     const renderTasks = () => {
         return tasks.map((item, index) => {
             return (<li className="relative m-4 border-2 border-dashed p-8" key={index}>
-                    <div className="absolute bottom-0 right-0 text-xl mr-2 text-indigo-200 drop-shadow-lg">
-                        {index+1}
-                    </div>
-                    {
-                        (idEdit !== item.index) ?
-                            <div className="text-3xl text-indigo-800 drop-shadow-lg drop-shadow-lg max-w-xs">{item.name}</div> :
-                            <input className="text-3xl text-indigo-800" type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)} />
-                    }
+                <div className="absolute bottom-0 right-0 text-xl mr-2 text-indigo-200 drop-shadow-lg">
+                    {index + 1}
+                </div>
+                {
+                    (idEdit !== item.index) ?
+                        <div className="text-3xl text-indigo-800 drop-shadow-lg drop-shadow-lg max-w-xs">{item.name}</div> :
+                        <input className="text-3xl text-indigo-800" type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} />
+                }
 
 
                 <div className="mt-8 flex justify-center">
@@ -72,22 +72,23 @@ const Todo = ({ avatar_url, login }) => {
         })
     }
 
-    return (<div className="bg-indigo-400 h-screen flex flex-col items-center p-8" >
-        <div className="flex items-center">
-            <h1 className= "m-4 text-indigo-800 text-4xl drop-shadow-lg">
-                <img className="rounded-full inline mr-4" src={avatar_url} width="80" />
-                Todo  for <span>{login} </span>
-            </h1>
-        </div>
-        {/* <h1 className="m-2 text-indigo-800 text-4xl drop-shadow-lg">Todo</h1> */}
-        <div className="flex items-center m-4">
-            <input className="rounded-lg text-green-800 text-4xl drop-shadow-lg pl-2 ml-2 mr-4" type="text" name="name" onChange={(e) => setName(e.target.value)} />
-            <button className="bg-green-600 text-indigo-200 hover:bg-green-500 p-2 rounded-lg" onClick={add} >Add</button>
-        </div>
-        <ul className="flex flex-wrap">
-            {renderTasks()}
-        </ul>
-    </div>)
+    return (
+        <div className="bg-indigo-400 h-screen flex flex-col items-center p-8" >
+            <div className="flex items-center">
+                <h1 className="m-4 text-indigo-800 text-4xl drop-shadow-lg">
+                    <img className="rounded-full inline mr-4" src={avatar_url} width="80" />
+                    Todo  for <span>{login} </span>
+                </h1>
+            </div>
+            {/* <h1 className="m-2 text-indigo-800 text-4xl drop-shadow-lg">Todo</h1> */}
+            <div className="flex items-center m-4">
+                <input className="rounded-lg text-green-800 text-4xl drop-shadow-lg pl-2 ml-2 mr-4" type="text" name="name" onChange={(e) => setName(e.target.value)} />
+                <button className="bg-green-600 text-indigo-200 hover:bg-green-500 p-2 rounded-lg" onClick={add} >Add</button>
+            </div>
+            <ul className="flex flex-wrap">
+                {renderTasks()}
+            </ul>
+        </div>)
 }
 
 Todo.getInitialProps = async (ctx) => {
